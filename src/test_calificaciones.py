55a4ecb81cfc37c36d7d547e0908cbacd22b1036
Manuel Jesús Benito Merchán <manbenmer1@alum.us.es>
2022-09-26 12:34:17 +0200

    Ejercicio1

---
+++ src/test_calificaciones.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+aciertos=23
+errores=3
+totalRespuestas=27
+N_aciertos=(aciertos*10)/totalRespuestas
+N_errrores=((errores*10)/(50-totalRespuestas))
+nota_Ada=(N_aciertos-N_errrores)
+
+print(nota_Ada)
+
